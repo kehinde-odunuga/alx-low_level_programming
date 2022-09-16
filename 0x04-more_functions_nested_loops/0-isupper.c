cc0b6056230b95b12c7dde1eb257ba3f6be736b9
kehinde-odunuga <omobolaji92@gmail.com>
2022-09-16 06:11:57 -0700

    is upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,8 @@
  * @c: The character to be checked
  * Return: 1 if the character is uppercase otherwise 0
  */
-
 int _isupper(int c)
 {
 	int is_upper = c >= 65 && c <= 90 ? 1 : 0;
-
 	return (is_upper);
 }
