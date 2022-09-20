5bba120f7b18845a10d0731483faa16ebceaec3f
Kehinde Odunuga <94455737+kehinde-odunuga@users.noreply.github.com>
2022-09-20 15:35:10 +0100

    initial commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_alphabet - prints the lowercase alphabets
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+		_putchar(low);
+
+	_putchar('\n');
+}
