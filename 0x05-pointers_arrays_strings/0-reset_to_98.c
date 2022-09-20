eb19dd8f4ba9152a28621d1b8bd88f932f9a4e5a
Kehinde Odunuga <94455737+kehinde-odunuga@users.noreply.github.com>
2022-09-20 11:14:52 +0100

    .

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,14 +1,8 @@
-/*
- * File: 0-reset_to_98.c
- * Auth: Brennan D Baraban
- */
-
-#include "main.h"
-
 /**
- * reset_to_98 - Takes a pointer to an int and updates
- *               the value it points to to 98.
- * @n: The pointer to an int.
+ * reset_to_98 - updates the value a pointer to an int points to to 98
+ * @n: integer pointed to
+ *
+ * Return: void
  */
 void reset_to_98(int *n)
 {
