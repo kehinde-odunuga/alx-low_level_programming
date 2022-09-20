b377c3a40bfcb680e59a75d494c0af04d25ad90a
Kehinde Odunuga <94455737+kehinde-odunuga@users.noreply.github.com>
2022-09-20 11:22:40 +0100

    puts2

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,40 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string s
+ *
+ * Return: length of s
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+	int i = 0;
+
+	/* if (s[0] == *s) */
+	/*	len++; */
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		len++;
+	}
+
+	return (len);
+}
+
+/**
+ * puts2 - prints every other character of a string,
+ * beginning with the first one, followed by a new line
+ * @str: string to print its every other character
+ *
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str); i = i + 2)
+		_putchar(str[i]);
+
+	_putchar('\n');
+}
