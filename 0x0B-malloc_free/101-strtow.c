5e1c5e2ff549879be92925545c21224656b457c3
kehinde-odunuga <omobolaji92@gmail.com>
2022-10-03 16:28:36 +0100

    update

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,99 +1,73 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
- * _isspace - Checks if a character is whitespace or '\0'
- * @c: Character to check
- *
- * Return: 1 if whitespace 0 otherwise
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
  */
-int _isspace(char c)
+int count_word(char *s)
 {
-	int i;
-	char whitespaces[] = " \t\n\v\f\r";
+int flag, c, w;
 
-	for (i = 0; i < 7; i++)
-	{
-		if (c == whitespaces[i])
-		{
-			return (1);
-		}
-	}
+flag = 0;
 
-	return (0);
+w = 0;
+for (c = 0; s[c] != '\0'; c++)
+{
+if (s[c] == ' ')
+flag = 0;
+else if (flag == 0)
+{
+flag = 1;
+w++;
+}
+}
+return (w);
 }
 
 /**
- * count_words - Count the number of words in a string
- * @str: The string in question
- *
- * Return: The number of words in str
+ * strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
  */
-int count_words(char *str)
-{
-	int words, in_word, i;
 
-	in_word = words = 0;
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (!_isspace(str[i]))
-		{
-			if (!in_word)
-			{
-				words++;
-				in_word = 1;
-			}
-		}
-		else
-		{
-			in_word = 0;
-		}
-	}
-
-	return (words);
-}
-
-/**
- * strtow - Converts a string to an array of words
- * @str: The string to split
- *
- * Return: A pointer to the array of words
- */
 char **strtow(char *str)
 {
-	int i, j, word_count;
-	char **words;
+char **matrix, *tmp;
 
-	if (str == NULL)
-		return (NULL);
-	word_count = count_words(str);
-	if (word_count == 0)
-		return (NULL);
-	words = malloc((sizeof(char *) * word_count) + 1);
-	if (words == NULL)
-		return (NULL);
-	j = 0;
-	for (i = 0; i < word_count; i++)
-	{
-		int wordlen, k;
+int i, k = 0, len = 0, words, c = 0, start, end;
 
-		while (_isspace(str[j]))
-			j++;
-		for (wordlen = 0; !(_isspace(str[wordlen + j])); wordlen++)
-			continue;
-		words[i] = malloc(wordlen + 1);
-		if (words[i] == NULL)
-		{
-			for (k = 0; k < i; k++)
-				free(words[k]);
-			free(words);
-		}
-		for (k = 0; k < wordlen; k++)
-		{
-			words[i][k] = str[j++];
-		}
-		words[i][wordlen] = '\0';
-	}
-	words[i] = NULL;
-	return (words);
+while (*(str + len))
+len++;
+words = count_word(str);
+if (words == 0)
+return (NULL);
+
+matrix = (char **) malloc(sizeof(char *) * (words + 1));
+if (matrix == NULL)
+return (NULL);
+for (i = 0; i <= len; i++)
+{
+if (str[i] == ' ' || str[i] == '\0')
+{
+if (c)
+{
+end = i;
+tmp = (char *) malloc(sizeof(char) * (c + 1));
+if (tmp == NULL)
+return (NULL);
+while (start < end)
+*tmp++ = str[start++];
+*tmp = '\0';
+matrix[k] = tmp - c;
+k++;
+c = 0;
+}
+}
+else if (c++ == 0)
+start = i;
+}
+matrix[k] = NULL;
+return (matrix);
 }
